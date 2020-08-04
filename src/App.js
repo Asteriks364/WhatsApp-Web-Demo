cb72700f2caadce83bdb32ea854ae12f5d4c3a73
Anatoliy <ansid10@yandex.ru>
2020-08-04 17:56:05 +0300

    Init App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return (
+		  <div className="app__wrapper">
+			  <div className="app__content"></div>
+		  </div>
+		);
+	}
+}
+
+export default App;
