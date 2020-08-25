fbc0a64e4596b4b41107b3bb80c51a894baa72ce
Anatoliy <t.sidorkin@citrus-soft.ru>
2020-08-25 16:58:01 +0300

    Просмотр контакта Вывод данных пользователя в Хедере вынесены в отдельный компонент

--- src/components/RightPanel/RightPanel.js
+++ src/components/RightPanel/RightPanel.js
@@ -1,38 +1,52 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import Context from '../../context/context';
 
 import { buttonClose } from '../Icons/buttonClose';
-import Search from '../Search/Search';
-import RightPanelMessageList from './RightPanelMessageList';
+import RightPanelSearchMessage from './RightPanelContent/RightPanelSearchMessage';
+import RightPanelOpenProfile from './RightPanelContent/RightPanelOpenProfile';
 import './RightPanel.css';
 
 export default function RightPanel() {
-  const { openRightPanel, setOpenRightPanel } = useContext(Context);
-
-  /* строка поиска по сообщениям */
-  const [searchMessage, setSearchMessage] = useState('');
+  const { actionRightPanel, setActionRightPanel } = useContext(Context);
 
   /* Закрытие правой панели */
-  const closePanel = () => setOpenRightPanel(false);
+  const closePanel = () => setActionRightPanel(false);
+
+  /* Заголовок правой панели */
+  const headerTitle = () => {
+    switch (actionRightPanel) {
+      case 'searchMessage':
+        return 'Поиск сообщений';
+      case 'openContactProfile':
+        return 'Данные контакта';
+      default:
+        return false;
+    }
+  };
+
+  /* Контент правой панели */
+  const panelContent = () => {
+    switch (actionRightPanel) {
+      case 'searchMessage':
+        return <RightPanelSearchMessage />;
+      case 'openContactProfile':
+        return <RightPanelOpenProfile />;
+      default:
+        return false;
+    }
+  };
 
   return (
     <div className="right-panel">
-      {openRightPanel && (
+      {actionRightPanel && (
         <div className="right-panel__wrap">
           <div className="right-panel__header">
             <div className="right-panel__close">
               <button onClick={closePanel}>{buttonClose()}</button>
             </div>
-            <div className="right-panel__title">Поиск сообщений</div>
+            <div className="right-panel__title">{headerTitle()}</div>
           </div>
-          <div className="right-panel__content">
-            <Search
-              search={searchMessage}
-              setSearch={setSearchMessage}
-              searchPlaceholder="Поиск..."
-            />
-            <RightPanelMessageList searchMessage={searchMessage} />
-          </div>
+          <div className="right-panel__content">{panelContent()}</div>
         </div>
       )}
     </div>
--- src/components/RightPanel/RightPanelContent/RightPanelMessageList.js
+++ src/components/RightPanel/RightPanelContent/RightPanelMessageList.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import Context from '../../context/context';
+import Context from '../../../context/context';
 
 import RightPanelMessage from './RightPanelMessage';
 
