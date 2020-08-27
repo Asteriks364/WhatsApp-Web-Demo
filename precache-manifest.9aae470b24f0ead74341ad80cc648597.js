0c580d7c5a0c8141e06643e0957bb33319717f22
Anatoliy <ansid10@yandex.ru>
2020-08-28 02:51:53 +0300

    Updates

---
+++ precache-manifest.9aae470b24f0ead74341ad80cc648597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50390abfa6c8dd8c17250ce0a1be821",
+    "url": "/WhatsApp-Web-Demo/index.html"
+  },
+  {
+    "revision": "0f56c22b04715b777232",
+    "url": "/WhatsApp-Web-Demo/static/css/main.3397a2fc.chunk.css"
+  },
+  {
+    "revision": "0eb3f838de9e801d64c8",
+    "url": "/WhatsApp-Web-Demo/static/js/2.52423810.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WhatsApp-Web-Demo/static/js/2.52423810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f56c22b04715b777232",
+    "url": "/WhatsApp-Web-Demo/static/js/main.bdc28a41.chunk.js"
+  },
+  {
+    "revision": "ab2f554f03840dc4615d",
+    "url": "/WhatsApp-Web-Demo/static/js/runtime-main.65cee227.js"
+  }
+]);
