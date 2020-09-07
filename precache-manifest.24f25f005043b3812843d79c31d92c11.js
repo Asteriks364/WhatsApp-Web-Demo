a1da4290c48f1373585e41d030d4171d0c03e544
Anatoliy <ansid10@yandex.ru>
2020-09-07 17:31:18 +0300

    Updates

---
+++ precache-manifest.24f25f005043b3812843d79c31d92c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e115155b5fa9da2983b0caa106d461",
+    "url": "/WhatsApp-Web-Demo/index.html"
+  },
+  {
+    "revision": "7bba160a76951d52ec14",
+    "url": "/WhatsApp-Web-Demo/static/css/main.1f87d907.chunk.css"
+  },
+  {
+    "revision": "410765a72441e0883b07",
+    "url": "/WhatsApp-Web-Demo/static/js/2.24731dd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WhatsApp-Web-Demo/static/js/2.24731dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bba160a76951d52ec14",
+    "url": "/WhatsApp-Web-Demo/static/js/main.8bdf7306.chunk.js"
+  },
+  {
+    "revision": "ab2f554f03840dc4615d",
+    "url": "/WhatsApp-Web-Demo/static/js/runtime-main.65cee227.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WhatsApp-Web-Demo/precache-manifest.a34b2c16271a164e699c40046b9e42a5.js"
+  "/WhatsApp-Web-Demo/precache-manifest.24f25f005043b3812843d79c31d92c11.js"
 );
 
 self.addEventListener('message', (event) => {
